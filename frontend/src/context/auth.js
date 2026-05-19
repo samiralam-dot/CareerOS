e6e5dccba27d1404b34068bc08a0d8ce3990d6b4
samir miyan <samiralam8748@gmail.com>
2026-05-19 12:26:36 +0530

    Update auth.js

--- frontend/src/context/auth.js
+++ frontend/src/context/auth.js
@@ -160,7 +160,7 @@ export const signOut = async () => {
 export const sendOtpMail = async (email, subject, html) => {
   try {
     const res = await axios.post(
-      https://career-os-d4ze.vercel.app/api/send-mail,
+      "https://career-os-d4ze.vercel.app/api/send-mail",
       {
         email,
         subject,
