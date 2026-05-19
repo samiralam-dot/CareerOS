a3cf5ef0bacbbaf1bb84eddc7180b30cc43a504a
Anonymous <samiralam8748@gmail.com>
2026-05-19 08:43:12 +0530

    "nodeMailer"

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -4,14 +4,18 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4, // force IPv4
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD
   }
 });
 
-transporter.verify((error) => {
+transporter.verify((error, success) => {
   if (error) {
     console.log("Mail Config Error:", error);
   } else {
