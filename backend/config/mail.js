9bc1d9ba557d08b0d92a577d217444a705eda542
Anonymous <samiralam8748@gmail.com>
2026-05-19 09:33:03 +0530

    "Dns"

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -6,20 +6,25 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD
   },
 
+  // Force IPv4
   lookup: (hostname, options, callback) => {
     dns.lookup(hostname, { family: 4 }, callback);
-  }
+  },
+
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000
 });
 
-transporter.verify((error) => {
+transporter.verify((error, success) => {
   if (error) {
     console.log("Mail Config Error:", error);
   } else {
